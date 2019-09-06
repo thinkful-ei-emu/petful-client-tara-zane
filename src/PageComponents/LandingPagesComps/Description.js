fca3b81cb24a64f562c2dcdb91a451431ab344b1
tarajeanne <t.jeanne.patel@gmail.com>
2019-09-06 15:16:34 -0500

    fixed picture:

--- src/PageComponents/LandingPagesComps/Description.js
+++ src/PageComponents/LandingPagesComps/Description.js
@@ -5,11 +5,11 @@ function Description (){
 
   return(
     <section className='LandingPageDescription'>
-      <p>Here at FIFO our goal is to make sure that animals dont have to wait to be adopted.
+      <p>Here at FIFO our goal is to make sure that animals dont have to wait to find their forever home.
         Our name stands for first in first out and thats our model for adoption.
         You can choose from a dog or cat, and the first pet of that type will be available to adopt.
         If you decide that particular pet isnt what your looking for we require you to start the process
-        over and head to the back of the line.We hope to find all of our animals happy loving homes here at FIFO.
+        over and head to the back of the line. We hope to find all of our animals happy loving homes here at FIFO.
       </p>
     </section>
   )
--- src/PageComponents/LandingPagesComps/LandingImage.js
+++ src/PageComponents/LandingPagesComps/LandingImage.js
@@ -4,7 +4,7 @@ import React from 'react';
 function LandingImage(){
   return (
     <div>
-      <img src="/" alt='Cat and Dog'/>
+      <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_706,w_1256,x_0,y_64/f_auto,q_auto,w_1100/v1554995050/shape/mentalfloss/516438-istock-637689912.jpg" alt='Cat and Dog'/>
     </div>
   )
 }
