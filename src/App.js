42cd2b02cd6d510e655fe96845a2d46ab854c2c6
Zane Kuehn <zanekuehn@yahoo.com>
2019-09-06 10:17:16 -0500

    Fixed App Breaking Typo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import LandingPage from './Pages/LandingPage';
 import {Route} from 'react-router-dom';
 
-class App extends React.component {
+class App extends React.Component {
 
 render(){
   return (
